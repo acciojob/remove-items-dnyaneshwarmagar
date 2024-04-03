b0196fbc2a256c3aebdd71428660ff78036870e5
Dnyaneshwar Magar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2024-04-03 06:31:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+let btn = document.querySelector("input");
+btn.addEventListener("click",removeColor)
+function removeColor() {
+      var selectElement = document.getElementById("colorSelect");
+      var selectedIndex = selectElement.selectedIndex;
+      
+      if (selectedIndex !== -1) { // Check if any option is selected
+        selectElement.remove(selectedIndex); // Remove the selected option
+      }
+    }
